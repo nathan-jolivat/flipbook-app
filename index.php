5ea01ac4f07cb877baa22182966a55164e364fe5
Benji <26485097+Mil00Z@users.noreply.github.com>
2019-04-03 13:23:29 +0200

    inté form + traitement php 80%

--- index.php
+++ index.php
@@ -1,25 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>Upload Image</title>
+        <title>Project - Upload Image</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/papy.css">
     </head>
     <body>
+    <main>
+
+
+
         <div class="container mt-5">
-            <form method="post" action="process.php" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="video">Uploader une vidéo</label>
-                    <input type="file" class="form-control" name="video" id="video">
-                    <small id="emailHelp" class="form-text text-muted">
-                        Le fichier vidéo ne doit pas excéder 8Mo
-                    </small>
-                </div>
-                <button type="submit" class="btn btn-primary"><i class="fa fa-cloud-upload-alt"></i> Upload</button>
-            </form>
+
+            <div class="row">
+
+
+
+                <form method="post" action="process.php" enctype="multipart/form-data" class="col-md-6">
+
+                    <div class="main-title-container">
+                        <h1 class="main-title">Générez votre FlipBook depuis une vidéo</h1>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="title" class="flipbook-title">Titre du flip book</label>
+                        <input type="text" class="form-control  custom-input custom-input-text"  name="title-video" id="title">
+
+                        <input type="file" class="form-control custom-input custom-input-file"  name="video" id="video">
+                        <small id="emailHelp" class="form-text text-muted">
+                            Le fichier vidéo ne doit pas excéder 8Mo
+                        </small>
+                    </div>
+                    <button type="submit" class="btn btn-primary custom-btn">Envoyer</button>
+                </form>
+
+                <div class="preview-content col-md-6"></div>
+
+            </div>
         </div>
+
+    </main>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="assets/js/main.js" async defer></script>
     </body>
 </html>
--- process.php
+++ process.php
@@ -1,3 +1,42 @@
 <?php
-var_dump($_POST['video']);
+//GET Data File
+$title_upload = $_POST['title-video'];
+$file_type = $_FILES['video']['type'];
+$file_size = $_FILES['video']['size'];
+$file_name = $_FILES['video']['name'];
+$file_tmp = $_FILES['video']['tmp_name'];
+
+//DEBEUG
+//    var_dump($file_type);
+//var_dump($file_name);
+////var_dump($file_size);
+    //var_dump($file_tmp);
+//$file_type = basename($file_type);
+$file_type = pathinfo($file_name);
+echo $file_type['extension'];
 die();
+
+//SET Type File OK
+$type_files = array('png','m4a','mp3','avi','mp4');
+
+if( $file_size <= 9000000) {
+    //    Taille ok
+        if(in_array($file_type, $type_files)){
+            //  Type file ok
+            $date = new DateTime();
+            $date = $date->format(Y-m-d);
+            var_dump($date);
+
+            $final_folder = $title_upload . $date ;
+            var_dump($final_folder);
+
+            move_uploaded_file($file_tmp,"$final_folder");
+        }
+        else {
+            echo 'le type de fichier est incorrect';
+        }
+}
+else {
+    echo "la taille est trop importante";
+}
+
