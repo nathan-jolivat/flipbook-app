5dd2a762de6f0bc6f7052eb9912f103b25d52550
LiliwoL <github@liliwol.fr>
2019-04-03 13:31:37 +0200

    t

--- extract_images_from_video.php
+++ extract_images_from_video.php
@@ -2,10 +2,10 @@
 
 // Source
 $video_src = "./video.mp4";
-
+$format = "565x328";
 
 // Commande
-$cmd = "/usr/local/bin/ffmpeg -i $video_src -vcodec mjpeg -vframes 1 -an -f rawvideo -s 640x360 -ss 10 './flipbook.jpg'";
+$cmd = "/usr/local/bin/ffmpeg -y -i $video_src -vcodec mjpeg -vframes 1 -an -f rawvideo -s $format -ss 10 './flipbook.jpg'";
 
 
 // Résultat
