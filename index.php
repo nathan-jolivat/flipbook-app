e5a74e111665b47574408a97e18a7158cc7ef21f
LeRomainMalin <remaudromain@gmail.com>
2019-04-03 14:17:15 +0200

    Deuxième commit avec une récupération des bases de chacun

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
         <title>Flipeo - Transformez vos vidéos en Flipboard</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/drop-zone.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/drop-zone.css">
     </head>
     <body>
         <div class="container mt-4">
             <div class="row">
                 <div class="logo animated zoomIn">
-                    <img src="img/logo.svg" alt="Flipeo Logo">
+                    <img src="assets/img/logo.svg" alt="Flipeo Logo">
                 </div>
                 <div class="col-md-6">
                     <div class="fingerprint-title animated fadeInUp">
@@ -31,7 +31,7 @@
                                 <div class="dropzone-wrapper animated zoomInUp">
                                     <div class="dropzone-desc">
                                         <i class="glyphicon glyphicon-download-alt"></i>
-                                        <img src="img/upload-illustration.png" alt="">
+                                        <img src="assets/img/upload-illustration.png" alt="">
                                         <p class="margin-top-10">Faites glisser votre vidéo ici</p>
                                     </div>
                                     <input type="file" name="video" class="dropzone">
@@ -49,7 +49,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="illustration animated fadeInRight">
-                        <img src="img/illustration.png" width="140%" alt="Illustration Flipeo">
+                        <img src="assets/img/illustration.png" width="140%" alt="Illustration Flipeo">
                     </div>
                 </div>
             </div>
@@ -57,6 +57,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="js/drop-zone.js"></script>
+    <script src="assets/js/drop-zone.js"></script>
     </body>
 </html>
