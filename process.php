ff8211d3da574f1aed4ca9cc9cfef82c2824f3ab
LiliwoL <github@liliwol.fr>
2019-04-04 09:27:34 +0200

    temp

--- process.php
+++ process.php
@@ -1,3 +1,40 @@
 <?php
-var_dump($_POST['video']);
-die();
+
+$name= $_FILES['file']['name'];
+
+$tmp_name= $_FILES['file']['tmp_name'];
+
+$position= strpos($name, ".");
+
+$fileextension= substr($name, $position + 1);
+
+$fileextension= strtolower($fileextension);
+
+
+if (isset($name)) {
+    $cleaname = filter_var($name,FILTER_SANITIZE_ENCODED);
+    $path= 'Uploads/videos/'.$cleaname;
+
+
+    if (empty($name))
+    {
+        echo "Pas de fichier selectionné";
+    }
+    else if (!empty($name)){
+        if (($fileextension !== "mp4"))
+        {
+            echo "L'extension doit être de format mp4";
+        }
+        else
+        {
+            if (!file_exists($path)) {
+                mkdir($path, 0777, true);
+            }
+            if (move_uploaded_file($tmp_name, $path.'/'.$name)) {
+                echo 'reçu!';
+            }
+
+        }
+    }
+}
+?>
