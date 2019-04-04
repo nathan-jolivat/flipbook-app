2cfb6accaab7542c14674a77794dca7194fe869a
manonLM <manon.lemerlus@viacesi.fr>
2019-04-04 09:25:11 +0200

    1er commit !!!!!

--- index.php
+++ index.php
@@ -4,19 +4,29 @@
         <title>Upload Image</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container mt-5">
-            <form method="post" action="process.php" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="video">Uploader une vidéo</label>
-                    <input type="file" class="form-control" name="video" id="video">
-                    <small id="emailHelp" class="form-text text-muted">
-                        Le fichier vidéo ne doit pas excéder 8Mo
-                    </small>
+        <div class="container">
+            <div class="row">
+                <div class="col-6 mt-2">
+                    <img src="assets/flipeo.png" alt="logo" class="img_logo">
+                    <h2>Générer votre FlipBook depuis une vidéo</h2>
+                    <form method="post" action="process.php" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <input type="file" class="form-control" name="file" id="video">
+                            <small id="emailHelp" class="form-text text-muted">
+                                Le fichier vidéo ne doit pas excéder 8Mo
+                            </small>
+                        </div>
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-cloud-upload-alt"></i> ENVOYER</button>
+                    </form>
                 </div>
-                <button type="submit" class="btn btn-primary"><i class="fa fa-cloud-upload-alt"></i> Upload</button>
-            </form>
+                <div class="col-6">
+                <img src="assets/flipbook.png" alt="img_home">
+                </div>
+            </div>
         </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- process.php
+++ process.php
@@ -1,3 +1,74 @@
 <?php
-var_dump($_POST['video']);
-die();
+
+function slugify($text)
+{
+    // Strip html tags
+    $text=strip_tags($text);
+    // Replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    // Transliterate
+    setlocale(LC_ALL, 'en_US.utf8');
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // Remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    // Trim
+    $text = trim($text, '-');
+    // Remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+    // Lowercase
+    $text = strtolower($text);
+    // Check if it is empty
+    if (empty($text)) { return 'n-a'; }
+    // Return result
+    return $text;
+}
+
+$name= $_FILES['file']['name'];
+
+$titre_clean = slugify($name);
+
+$tmp_name= $_FILES['file']['tmp_name'];
+
+$position= strpos($name, ".");
+
+$fileextension= substr($name, $position + 1);
+
+$fileextension= strtolower($fileextension);
+
+
+if (isset($name)) {
+    $path= 'Uploads/videos/'.$titre_clean.'/';
+    if (empty($name))
+    {
+        echo "Choissisez un fichier";
+    }
+    else if (!empty($name)){
+        if ($fileextension !== "mp4")
+        {
+            echo "L'extension du fichier n'est pas bonne (seulement .mp4)";
+        }
+        else if ($fileextension == "mp4")
+        {
+            if (!file_exists($path)){
+                mkdir($path, 0777, true);
+            }
+            if (move_uploaded_file($tmp_name, $path."/".$name))
+            {
+                echo 'Uploaded!';
+            }
+        }
+    }
+}
+
+/*
+if ($fileextension == "mp4")
+{
+    echo "<video width='320' controls>
+    <source src='$path/$name' type='video/$fileextension'>
+        Votre navigateur ne supporte pas les vidéos.
+    </video>";
+}*/
+
+extractFlips ( "./video.mp4" );
+
+?>
