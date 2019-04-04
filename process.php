e602b0df8e06c966c7f7d8db4e94bb837b0687ab
Nathan JOLIVAT <nathan@enjoyup.io>
2019-04-04 10:04:25 +0200

    Correction d'une erreur de logique dans le process

--- process.php
+++ process.php
@@ -43,7 +43,6 @@ if (is_dir($uploads_dir)) {
     $name = $_FILES["video"]["name"];
     mkdir("$uploads_dir/$dir_name", 0777, true);
     $temp_dir = $uploads_dir . "/" . $dir_name;
-    move_uploaded_file($_FILES["video"]["tmp_name"], "$temp_dir/$titre_video_clean");
 
     if ( move_uploaded_file($_FILES["video"]["tmp_name"], "$temp_dir/$titre_video_clean")) {
         extractFlips("$temp_dir/$titre_video_clean");
